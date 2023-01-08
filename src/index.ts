0a21e6d22989f8d6580b62d2f1d6b258f07b1b34
tom <toms@live.jp>
2023-01-09 08:05:56 +0900

    Redis constructor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import IORedis from "ioredis";
+
 export const test = 1;
+
+export class Redis {
+  protected port: number;
+  protected host: string;
+  protected redis: IORedis | undefined;
+  constructor(port: number, host: string) {
+    this.port = port;
+    this.host = host;
+    this.redis = undefined;
+  }
+}
