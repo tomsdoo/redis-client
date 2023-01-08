81f75f076b3b58c86614d0bf46f69467272ee804
tom <toms@live.jp>
2023-01-09 08:06:25 +0900

    test for Redis constructor

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -6,10 +6,34 @@ import {
   expect,
   jest,
 } from "@jest/globals";
-import { test } from "@/index";
+import { Redis, test } from "@/index";
 
 describe("test", () => {
   it("value is 1", () => {
     expect(test).toBe(1);
+  });
+});
+
+describe("Redis class", () => {
+  describe("constructor", () => {
+    let instance: Redis;
+
+    beforeEach(() => {
+      instance = new Redis(6379, "localhost");
+    });
+
+    afterEach(() => {});
+
+    it("instance has port", () => {
+      expect(instance).toHaveProperty("port", 6379);
+    });
+
+    it("instance has host", () => {
+      expect(instance).toHaveProperty("host", "localhost");
+    });
+
+    it("instance has redis", () => {
+      expect(instance).toHaveProperty("redis", undefined);
+    });
   });
 });
