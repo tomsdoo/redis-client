b9eea286e8f4ca1085a11f9fa647c8378c1c7205
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2023-01-09 11:55:46 +0900

    config keyPrefix (#14)
    * config.keyPrefix
    
    * test for config.keyPrefix

--- __test__/index.constructor.test.ts
+++ __test__/index.constructor.test.ts
@@ -15,6 +15,7 @@ describe("Redis class", () => {
     instance = new Redis({
       port: 6379,
       host: "localhost",
+      keyPrefix: "some",
     });
   });
 
@@ -27,8 +28,12 @@ describe("Redis class", () => {
       expect(instance.config).toHaveProperty("port", 6379);
     });
 
-    it("instance has host", () => {
+    it("instance has config.host", () => {
       expect(instance.config).toHaveProperty("host", "localhost");
+    });
+
+    it("instance has config.keyPrefix", () => {
+      expect(instance.config).toHaveProperty("keyPrefix", "some");
     });
 
     it("instance has redis", () => {
