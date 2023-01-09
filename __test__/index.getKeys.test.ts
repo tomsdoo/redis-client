06fe0820671b89a15fa98e0031e2f93136f9f555
tom <toms@live.jp>
2023-01-09 14:52:19 +0900

    test for getKeys()

---
+++ __test__/index.getKeys.test.ts
@@ -0,0 +1,80 @@
+import { beforeEach, describe, it, expect, jest } from "@jest/globals";
+import { Redis } from "@/index";
+
+const counter: {
+  ioredis: {
+    keys: string[];
+  };
+  reset: Function;
+} = {
+  ioredis: {
+    keys: [],
+  },
+  reset: () => {
+    counter.ioredis.keys = [];
+  },
+};
+
+jest.mock(
+  "ioredis",
+  () =>
+    class IORedis {
+      protected port: number;
+      protected host: string;
+      protected keyPrefix: string;
+      constructor({
+        port,
+        host,
+        keyPrefix,
+      }: {
+        port: number;
+        host: string;
+        keyPrefix: string;
+      }) {
+        this.port = port;
+        this.host = host;
+        this.keyPrefix = keyPrefix;
+      }
+
+      public async keys(filterKey: string): Promise<string[]> {
+        counter.ioredis.keys.push(filterKey);
+        return await Promise.resolve(
+          Array.from(Array(2), (v, i) => i).map(
+            (i) => `${filterKey.replace(/\*/g, "")}${i}`
+          )
+        );
+      }
+    }
+);
+
+describe("Redis class", () => {
+  let instance: Redis;
+  beforeEach(() => {
+    counter.reset();
+  });
+
+  describe("getKeys()", () => {
+    it("keyPrefix does not exist", async () => {
+      instance = new Redis({
+        port: 6379,
+        host: "localhost",
+      });
+
+      expect(await instance.getKeys()).toEqual(["0", "1"]);
+      expect(counter.ioredis.keys.length).toBe(1);
+      expect(counter.ioredis.keys.slice(-1)[0]).toBe("*");
+    });
+
+    it("keyPrefix exists", async () => {
+      instance = new Redis({
+        port: 6379,
+        host: "localhost",
+        keyPrefix: "testkey",
+      });
+
+      expect(await instance.getKeys()).toEqual(["0", "1"]);
+      expect(counter.ioredis.keys.length).toBe(1);
+      expect(counter.ioredis.keys.slice(-1)[0]).toBe("testkey*");
+    });
+  });
+});
