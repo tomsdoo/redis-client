93440e44ad176a382647b4d14f9fc235ce35e344
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2023-01-14 13:19:22 +0900

    set() interface (#22)
    * uuid is installed
    
    * keyProp is added
    
    * target in tsconfig is specified
    
    * keyProp is added
    
    * test for set() is changed for keyProp addition

--- __test__/index.constructor.test.ts
+++ __test__/index.constructor.test.ts
@@ -11,35 +11,77 @@ import { Redis } from "@/index";
 describe("Redis class", () => {
   let instance: Redis;
 
-  beforeEach(() => {
-    instance = new Redis({
-      keyPrefix: "some",
-      options: {
-        port: 6379,
-        host: "localhost",
-      },
-    });
-  });
-
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   describe("constructor", () => {
-    it("instance has config.options.port", () => {
-      expect(instance.config).toHaveProperty("options.port", 6379);
+    describe("without some", () => {
+      beforeEach(() => {
+        instance = new Redis({
+          keyPrefix: "some",
+          options: {
+            port: 6379,
+            host: "localhost",
+          },
+        });
+      });
+
+      it("instance has config.options.port", () => {
+        expect(instance.config).toHaveProperty("options.port", 6379);
+      });
+
+      it("instance has config.options.host", () => {
+        expect(instance.config).toHaveProperty("options.host", "localhost");
+      });
+
+      it("instance has config.keyPrefix", () => {
+        expect(instance.config).toHaveProperty("keyPrefix", "some");
+      });
+
+      it("instance has redis", () => {
+        expect(instance).toHaveProperty("redis", undefined);
+      });
+
+      it("instance has keyProp as default value", () => {
+        expect(instance.keyProp).toBe("_id");
+      });
+
+      it("instance has expireSeconds as default value", () => {
+        expect(instance.expireSeconds).toBe(undefined);
+      });
     });
 
-    it("instance has config.options.host", () => {
-      expect(instance.config).toHaveProperty("options.host", "localhost");
+    describe("with keyProp", () => {
+      beforeEach(() => {
+        instance = new Redis({
+          keyProp: "keyProp",
+          options: {
+            port: 6379,
+            host: "localhost",
+          },
+        });
+      });
+
+      it("instance has keyProp", () => {
+        expect(instance.keyProp).toBe("keyProp");
+      });
     });
 
-    it("instance has config.keyPrefix", () => {
-      expect(instance.config).toHaveProperty("keyPrefix", "some");
-    });
+    describe("with expireSeconds", () => {
+      beforeEach(() => {
+        instance = new Redis({
+          options: {
+            port: 6379,
+            host: "localhost",
+          },
+          expireSeconds: 10,
+        });
+      });
 
-    it("instance has redis", () => {
-      expect(instance).toHaveProperty("redis", undefined);
+      it("instance has expireSeconds", () => {
+        expect(instance.expireSeconds).toBe(10);
+      });
     });
   });
 });
--- __test__/index.set.test.ts
+++ __test__/index.set.test.ts
@@ -27,6 +27,10 @@ const counter: {
   },
 };
 
+jest.mock("uuid", () => ({
+  v4: () => "dummyId",
+}));
+
 jest.mock(
   "ioredis",
   () =>
@@ -57,37 +61,65 @@ jest.mock(
 
 describe("Redis class", () => {
   describe("set()", () => {
-    let instance: Redis;
-    beforeEach(() => {
-      instance = new Redis({
-        options: {
-          port: 6379,
-          host: "localhost",
-        },
+    interface TestType {
+      key: string;
+      message: string;
+    }
+
+    let instance: Redis<TestType>;
+
+    describe("expireSeconds is not set", () => {
+      beforeEach(() => {
+        instance = new Redis({
+          options: {
+            port: 6379,
+            host: "localhost",
+          },
+          keyProp: "key",
+        });
+      });
+
+      afterEach(() => {
+        counter.reset();
+      });
+
+      it("not expires", async () => {
+        expect(await instance.set({ message: "test" })).toEqual({
+          key: "dummyId",
+          message: "test",
+        });
+        expect(counter.ioredis.set.length).toBe(1);
+        expect(counter.ioredis.set.slice(-1)[0]).toEqual({
+          key: "dummyId",
+          value: JSON.stringify({ key: "dummyId", message: "test" }),
+        });
       });
     });
 
-    afterEach(() => {
-      counter.reset();
-    });
-
-    it("expires", async () => {
-      expect(await instance.set("key", { message: "test" }, 2)).toBe("OK");
-      expect(counter.ioredis.set.length).toBe(1);
-      expect(counter.ioredis.set.slice(-1)[0]).toEqual({
-        key: "key",
-        value: JSON.stringify({ message: "test" }),
-        expireType: "EX",
-        expires: 2,
+    describe("expireSeconds is set", () => {
+      beforeEach(() => {
+        instance = new Redis({
+          options: {
+            port: 6379,
+            host: "localhost",
+          },
+          expireSeconds: 10,
+          keyProp: "key",
+        });
       });
-    });
 
-    it("not expires", async () => {
-      expect(await instance.set("key", { message: "test" })).toBe("OK");
-      expect(counter.ioredis.set.length).toBe(1);
-      expect(counter.ioredis.set.slice(-1)[0]).toEqual({
-        key: "key",
-        value: JSON.stringify({ message: "test" }),
+      it("expires", async () => {
+        expect(await instance.set({ message: "test" })).toEqual({
+          key: "dummyId",
+          message: "test",
+        });
+        expect(counter.ioredis.set.length).toBe(1);
+        expect(counter.ioredis.set.slice(-1)[0]).toEqual({
+          key: "dummyId",
+          value: JSON.stringify({ key: "dummyId", message: "test" }),
+          expireType: "EX",
+          expires: 10,
+        });
       });
     });
   });
