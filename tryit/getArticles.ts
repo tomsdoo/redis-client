65fe241a984c2497ef59a64e3a69aa417d403704
tom <toms@live.jp>
2023-02-17 07:45:23 +0900

    tryit getArticles

---
+++ tryit/getArticles.ts
@@ -0,0 +1,13 @@
+import { clients } from "./common";
+
+(async () => {
+  const keys = await clients.articleRedis.getKeys();
+
+  for(const key of keys) {
+    console.log(
+      await clients.articleRedis.get(key)
+    );
+  }
+})()
+  .then(() => process.exit())
+  .catch(() => process.exit());
