61a7efe87e3ee305aee5541a3f0092a7d81dd844
tomsdoo <toms@live.jp>
2025-10-30 06:55:00 +0900

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,36 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  test: {
+    projects: [
+      {
+        test: {
+          include: ["__test__/**/*.test.ts"],
+          exclude: ["__test__/**/*.local.test.ts"],
+          name: "node",
+          environment: "node",
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@@": fileURLToPath(new URL("./", import.meta.url)),
+          },
+        },
+      },
+      {
+        test: {
+          include: ["__test__/**/*.local.test.ts"],
+          name: "local",
+          environment: "node",
+          setupFiles: ["./vitest.local.setup.ts"],
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@@": fileURLToPath(new URL("./", import.meta.url)),
+          },
+        },
+      },
+    ],
   },
 });
--- vitest.local.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-import defaultConfig from "./vitest.config";
-
-export default defineConfig({
-  ...defaultConfig,
-  test: {
-    setupFiles: ["./vitest.setup.ts"],
-  },
-});
